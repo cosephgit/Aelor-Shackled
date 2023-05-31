c6d305688664ad4ef4ccb8ae1af27109f3954ab3
Skylar Masson <32114503+Smasson76@users.noreply.github.com>
2023-05-30 23:19:22 -0500

    Made battlecontroller comments; changed player and enemy script names

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -1,43 +1,58 @@
+/****************************************************************************
+ * Author:			Skylar Masson
+ * Date started:	4/26/2023
+ * Description:  	Class contains all methods for the Battle Sequence
+ ******************************************************************************/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//This class is for managing the battle sequence
 public class BattleManager : MonoBehaviour {
 
-    public static BattleManager instance;
+    public static BattleManager instance;   //Creating a singleton so each script can call it without having to assign it to each class that uses it.
 
-    EventBattle callingEvent;
+    [SerializeField] EventBattle callingEvent;  //Store the current battle even
 
-    [Header("--PUBLIC GAMEOBJECTS--")]
+    [Header("--PUBLIC GAMEOBJECTS--")]  //All public Gameobjects
     public GameObject player;
     public GameObject enemy;
 
-    [Header("--PUBLIC CANVAS--")]
+    [Header("--PUBLIC CANVAS--")]       //All public canvas's
     public GameObject playerCanvas;
     public GameObject enemyCanvas;
 
+    //Initial method that sets the instance to only this class
     void Awake() {
         if (instance == null) instance = this;
     }
 
+    //Method at start sets player and enemy variables; disables battle-related controllers and canvas
     void Start() {
-        player = GameObject.FindWithTag("Player");
-        enemy = GameObject.FindWithTag("Enemy");
+        player = GameObject.FindWithTag("Player");  //Set player
+        enemy = GameObject.FindWithTag("Enemy");    //Set enemy
 
         player.GetComponent<PlayerController>().enabled = false;
         enemy.GetComponent<EnemyController>().enabled = false;
 
+        //Set battle canvas inactive
         playerCanvas.SetActive(false);
         enemyCanvas.SetActive(false);
 
         BeginBattleEvent(callingEvent);
     }
     
+    //Method that is called when the Battle begins
+    //Takes in parameter to set class variable to it
     public void BeginBattleEvent(EventBattle callingEventNew) {
         callingEvent = callingEventNew;
+
+        //Enable player and enemy battle controllers
         player.GetComponent<PlayerController>().enabled = true;
         enemy.GetComponent<EnemyController>().enabled = true;
 
+        //Set player and enemy canvas active
         playerCanvas.SetActive(true);
         enemyCanvas.SetActive(true);
 
@@ -45,9 +60,11 @@ public class BattleManager : MonoBehaviour {
     }
 
     public void BattleEndsVictory() {
+        //Disable player and enemy battle controllers
         player.GetComponent<PlayerController>().enabled = false;
         enemy.GetComponent<EnemyController>().enabled = false;
 
+        //Set player and enemy canvas inactive
         playerCanvas.SetActive(false);
         enemyCanvas.SetActive(false);
 
@@ -57,9 +74,11 @@ public class BattleManager : MonoBehaviour {
     }
 
     public void BattleEndsDefeat() {
+        //Disable player and enemy battle controllers
         player.GetComponent<PlayerController>().enabled = false;
         enemy.GetComponent<EnemyController>().enabled = false;
 
+        //Set player and enemy canvas inactive
         playerCanvas.SetActive(false);
         enemyCanvas.SetActive(false);
 
--- Assets/Scripts/PlayerBattleController.cs
+++ Assets/Scripts/PlayerBattleController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerBattleController : MonoBehaviour {
 
     [Header("--PUBLIC PLAYER DATA--")]
     public float fireboltVelocity, frostBeamVelocity;
